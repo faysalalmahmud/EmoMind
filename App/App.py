465ec0d824974a8e837b54f793d81a0d1f403b2e
Faysal Al Mahmud <99035760+faysalalmahmud@users.noreply.github.com>
2024-11-27 10:48:38 +0600

    Update App.py

--- App/App.py
+++ App/App.py
@@ -11,11 +11,13 @@ import numpy as np
 
 #Utils
 import joblib
+from streamlit import image
+
 # from streamlit import columns
 
 
 #Function Call
-pipe_lr = joblib.load(open("Model/emotion_classifier_pipe_lr_2.pkl", "rb"))
+pipe_lr = joblib.load(open("E:\Project\Emotion-Detection-From-Text\Model\emotion_classifier_pipe_lr_2.pkl", "rb"))
 
 def predict_emotion(docx):
     results = pipe_lr.predict([docx])
@@ -36,38 +38,42 @@ def main():
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
+        # st.snow()
         st.subheader("Home \nEmotion In Text")
         with st.form(key='emotion_clf_form'):
             raw_text = st.text_area("Enter your text", key='emotion_clf_text')
             submit_text = st.form_submit_button("Submit")
-        if submit_text:
+        if submit_text and raw_text != '':
             col1, col2 = st.columns(2)
+
 
             #Apply Function Here
             prediction = predict_emotion(raw_text)
             probability = get_prediction_proba(raw_text)
 
             with col1:
-                st.success("Original Text")
-                st.write(raw_text)
+                # with st.container(border=2):
+                #     st.success("Original Text")
+                #     st.write(raw_text)
 
-                st.success("Predicted")
-                emoji_icon = emotions_emoji_dict[prediction]
-                st.write("{}:{}".format(prediction, emoji_icon))
-                st.write("Confidence:{}".format(np.max(probability)))
+
+                with st.container(border=2):
+                    st.success("Prediction")
+                    emoji_icon = emotions_emoji_dict[prediction]
+                    st.write("{}:{}".format(prediction, emoji_icon))
+                    st.write("Confidence:{}".format(np.max(probability)))
 
             with col2:
-                st.success("Predicted Probability")
-                # st.write(probability)
-                proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
-                # st.write(proba_df.T)
-                proba_df_clean = proba_df.T.reset_index()
-                proba_df_clean.columns = ["emotions", "probability"]
-
-
-                # fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability')
-                fig = px.bar(proba_df_clean, x='emotions', y='probability')
-                st.plotly_chart(fig, use_container_width=True)
+                with st.container(border=2):
+                    st.success("Predicted Probability")
+                    # st.write(probability)
+                    proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
+                    # st.write(proba_df.T)
+                    proba_df_clean = proba_df.T.reset_index()
+                    proba_df_clean.columns = ["Emotions", "Probability"]
+                    # fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability')
+                    fig = px.bar(proba_df_clean, x='Emotions', y='Probability')
+                    st.plotly_chart(fig, use_container_width=True)
 
 
 
